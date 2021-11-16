68d0c2a44200e4b917d34d0a7e33316d971e9cc7
Emilio Resende <emilio.murta@msitbrasil.com>
2021-11-16 19:32:19 -0300

    + forms done right (uncontrolled components)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import ImcView from "./views/ImcView";
 import Person from "./domain/Person";
 
 export default function App() {
   const [person, setPerson] = useState(new Person(0.1, 0.1));
+  const heightElem = useRef();
+  const weightElem = useRef();
 
-  const calculate = function () {
-    const heightElem = document.querySelector("#height");
-    const weightElem = document.querySelector("#weight");
-
-    if (!heightElem) throw Error("height can't be found.");
-    if (!weightElem) throw Error("height can't be found.");
+  const calculate = function (evt) {
+    evt.preventDefault();
 
     const newPerson = new Person(
-      parseFloat(heightElem.value),
-      parseFloat(weightElem.value)
+      parseFloat(heightElem.current.value),
+      parseFloat(weightElem.current.value)
     );
 
     setPerson(newPerson);
@@ -24,25 +22,24 @@ export default function App() {
   return (
     <div className="container">
       <div className="data">
-        <div className="form">
+        <form className="form" onSubmit={calculate}>
           <div className="row">
             <label>Altura</label>
-            <input id="height" name="height" placeholder="0.00" />
+            <input id="height" ref={heightElem} placeholder="0.00" />
           </div>
           <div className="row">
             <label>Peso</label>
-            <input id="weight" placeholder="0.00" />
+            <input id="weight" ref={weightElem} placeholder="0.00" />
           </div>
           <div className="actions">
             <button
-              type="button"
+              type="submit"
               className="primary-action"
-              onClick={calculate}
             >
               Calcular
             </button>
           </div>
-        </div>
+        </form>
       </div>
       <ImcView className="result" person={person} />
     </div>
